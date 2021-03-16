974fa5219b342c17ca68f130cc825ed67aa9bdab
Captain1045 <amalnc055@gmail.com>
2021-03-16 13:05:02 +0530

    data binding

--- bankApp/src/app/app-routing.module.ts
+++ bankApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'registration', component: RegistrationComponent
+  },
+  {
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bankApp/src/app/login/login.component.ts
+++ bankApp/src/app/login/login.component.ts
@@ -12,13 +12,43 @@ export class LoginComponent implements OnInit {
     1002: { accno: 1002, name: "Ajay", bal: 7000, pass: "user3" },
     1003: { accno: 1003, name: "Hari", bal: 6000, pass: "user4" },
   }
-
+  // temp_data="Testing by Interpolation";
+  user_acc="";
+  user_pwd="";
   constructor() { }
 
   ngOnInit(): void {
   }
+  getUsername(event: any)
+  {
+    this.user_acc=event.target.value;
+    console.log(this.user_acc);
+  }
+  getPassword(event: any)
+  {
+    this.user_pwd=event.target.value;
+    console.log(this.user_pwd);
+  }
   login() {
     alert("log in works");
-    
+    // let user_acc = document.querySelector("#user_accno").value;
+    // let user_pwd = document.querySelector("#user_pass").value;
+    // var data = bank.getAccountDetails();
+    // if (user_acc in data) {
+    //   //console.log(data[user_acc]["pass"]);
+    //   //alert(user_acc);
+    //   if (user_pwd == data[user_acc]["pass"]) {
+    //     alert("Authentication Successful");
+    //     window.location.href = "userhome.html";
+    //   }
+    //   else {
+    //     alert("Password Mismatch");
+    //   }
+
+    // }
+    // else {
+    //   alert("Invalid Account Number!");
+    // }
+
   }
 }
