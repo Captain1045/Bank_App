bf6ed0d99787ff99f335fa0dc80f415b6f618029
Captain1045 <amalnc055@gmail.com>
2021-04-08 00:01:31 +0530

    session work

--- bankServer/index.js
+++ bankServer/index.js
@@ -1,23 +1,64 @@
 //const { json } = require("express");
 const express = require("express");
+const session = require('express-session');
 const dataService = require("./services/data_service");
 const app = new express();
+var currentUser;
 app.use(express.json());
+const logMiddleware = (req, res, next) => {
+    console.log(req.body);
+    next();
+}
+app.use(logMiddleware);
+const authenticateMiddleware=(req,res,next)=>{
+    if (req.session.currentUser != accountDetails[req.body.accno].name) {
+        return res.json({
+            status: false,
+            statusCode: 422,
+            message: "Please Log in!"
+        })
+    }
+    else
+    {
+    next();
+    }
+}
+app.use(session({
+    secret: "randomsecurestring",
+    resave: false,
+    saveUninitialized: false
+}));
 app.get("/", (req, res) => {
     res.send("Welcome GET");
 });
 app.post("/login", (req, res) => {
-    
-    const result=dataService.login(req.body.accno,req.body.pass);
-    console.log(res.send(result.message));
-    res.send("Welcome login GET");
+
+    const result = dataService.login(req, req.body.accno, req.body.pass);
+    console.log(res.status(result.statusCode).json(result));
+    //currentUser=res.send(result.currentUser);
+    // res.send("Welcome login GET");
 
 });
 app.post("/register", (req, res) => {
     console.log(req.body);
     const result = dataService.registerData(req.body.accno, req.body.name, req.body.bal, req.body.pass);
-    console.log(res.send(result.message));
-    res.send("Welcome register POST");
+    console.log(res.status(result.statusCode).json(result));
+    // console.log(res.send(result.message));
+    // res.send("Welcome register POST");
+});
+app.post("/deposit",authenticateMiddleware, (req, res) => {
+    console.log(req.body);
+    const result = dataService.deposit(req, req.body.accno, req.body.pass, req.body.amount);
+    console.log(res.status(result.statusCode).json(result));
+    // console.log(res.send(result.message));
+    // res.send("Welcome register POST");
+});
+app.post("/withdraw",authenticateMiddleware, (req, res) => {
+    console.log(req.body);
+    const result = dataService.withdraw(req, req.body.accno, req.body.pass, req.body.amount);
+    console.log(res.status(result.statusCode).json(result));
+    // console.log(res.send(result.message));
+    // res.send("Welcome register POST");
 });
 //app.post("/", (req, res) => {
 //     res.send("Welcome POST");
--- bankServer/services/data_service.js
+++ bankServer/services/data_service.js
@@ -1,3 +1,4 @@
+
 accountDetails = {
     1001: { accno: 1001, name: "Amal", bal: 3000, pass: "user1" },
     1002: { accno: 1002, name: "Arun", bal: 5000, pass: "user2" },
@@ -9,6 +10,7 @@ const registerData = (accno, name, bal, pass) => {
     if (accno in accountDetails) {
         return {
             status: false,
+            statusCode: 422,
             message: "User Exists! Please Login "
         }
         // alert("User Exists! Please Login ");
@@ -24,6 +26,7 @@ const registerData = (accno, name, bal, pass) => {
         //this.saveDetails();
         return {
             status: true,
+            statusCode: 200,
             message: "Registration Successful!"
         }
         // alert(`Registration Successful!\n`)
@@ -31,18 +34,20 @@ const registerData = (accno, name, bal, pass) => {
     }
     //return true;
 }
-const login = (acc, pwd) => {
+const login = (req, acc, pwd) => {
     if (acc in accountDetails) {
         //console.log(data[user_acc]["pass"]);
         //alert(this.user_acc);
         if (pwd == accountDetails[acc]["pass"]) {
-            //this.currentUser=accountDetails[acc].name;
+            req.session.currentUser = accountDetails[acc].name;
             // this.saveDetails();
             // alert("Authentication Successful");
             // //window.location.href = "dashboard";
             // this.router.navigateByUrl("dashboard");
             return {
                 status: true,
+                statusCode: 200,
+                //currentUser: req.session.currentUser,
                 message: "Authentication Successful"
             }
         }
@@ -50,6 +55,7 @@ const login = (acc, pwd) => {
             //alert("Incorrect Password!");
             return {
                 status: false,
+                statusCode: 422,
                 message: "Incorrect Password!"
             }
         }
@@ -59,11 +65,108 @@ const login = (acc, pwd) => {
         //alert("Invalid Account Number!");
         return {
             status: false,
+            statusCode: 422,
             message: "Invalid Account Number!"
+        }
+    }
+}
+const deposit = (req, acc_no, password, amount) => {
+    if (req.session.currentUser != accountDetails[acc_no].name) {
+        return {
+            status: false,
+            statusCode: 422,
+            message: "Please Log in!"
+        }
+    }
+    else {
+        let data = accountDetails;
+        if (acc_no in data) {
+            // alert(data[acc_no].pass);
+            // alert(password);
+            if ((data[acc_no].pass == password)) {//&&(data[acc_no].name==this.currentUser)) {
+                data[acc_no].bal += amount;
+                return {
+                    status: true,
+                    statusCode: 200,
+                    message: "Available Balance = " + data[acc_no].bal
+                }
+                //alert("Available Balance = " + data[acc_no].bal);
+            }
+            else {
+                return {
+                    status: false,
+                    statusCode: 422,
+                    message: "Incorrect data! Please try again"
+                }
+                //alert("Incorrect data! Please try again");
+            }
+        }
+
+        else {
+            return {
+                status: false,
+                statusCode: 422,
+                message: "No User exist! Please try again"
+            }
+            //alert("No User exist! Please try again");
+        }
+    }
+}
+const withdraw = (req, acc_no, password, amount) => {
+    if (req.session.currentUser != accountDetails[acc_no].name) {
+        return {
+            status: false,
+            statusCode: 422,
+            message: "Please Log in!"
+        }
+    }
+    else {
+        let data = accountDetails;
+        if (acc_no in data) {
+            if ((data[acc_no].pass == password)) {//&&(data[acc_no].name==this.currentUser)) {
+                if (data[acc_no].bal < amount) {
+
+                    return {
+                        status: false,
+                        statusCode: 422,
+                        message: "Insufficient Balance   Available Balance = " + data[acc_no].bal
+                    }
+                    // alert("Insufficient Balance\nAvailable Balance = " + data[acc_no].bal);
+                }
+                else {
+                    data[acc_no].bal -= amount;
+                    return {
+                        status: true,
+                        statusCode: 200,
+                        message: "Available Balance = " + data[acc_no].bal
+                    }
+                    //alert("Available Balance = " + data[acc_no].bal);
+                }
+
+            }
+            else {
+                return {
+                    status: false,
+                    statusCode: 422,
+                    message: "Incorrect data! Please try again"
+                }
+                //alert("Incorrect data! Please try again");
+            }
+
+        }
+        else {
+            return {
+                status: false,
+                statusCode: 422,
+                message: "No User exist! Please try again"
+            }
+            //alert("No User exist! Please try again");
         }
     }
 }
 module.exports = {
     registerData,
-    login
+    login,
+    deposit,
+    withdraw
 }
